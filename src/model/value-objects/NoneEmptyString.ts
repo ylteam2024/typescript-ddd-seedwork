c307ac4661941ce83a8daaf4f80e81a70358e53d
p77u4n <tuancr.pt@gmail.com>
2023-10-11 15:51:41 +0700

    add NoneEmptyString10 and NoneEmptyString100

--- src/model/value-objects/NoneEmptyString.ts
+++ src/model/value-objects/NoneEmptyString.ts
@@ -1,6 +1,7 @@
 import { BaseExceptionBhv } from '@logic/exception.base';
 import { Either, NEA, flow } from '@logic/fp';
 import { PrimitiveVOTrait } from '@model/value-object.base';
+import { Brand } from '@type_util/index';
 
 export const isEmptyStringMaxNLength =
   <T>(maxLength: number) =>
@@ -33,3 +34,21 @@ export function getTrait<T>({
 export const NEStringAuFn = {
   getTrait,
 };
+
+export type NonEmptyStringMax10 = Brand<string, 'NonEmptyStringMax10'>;
+
+export type NonEmptyStringMax100 = Brand<string, 'NonEmptyStringMax100'>;
+
+export const nonEmptyStringMax10Trait =
+  NEStringAuFn.getTrait<NonEmptyStringMax10>({
+    max: 10,
+    message: 'should not be empty and large than 10',
+    exceptionCode: 'INVALID_NON_EMPTY_STRING_MAX_10',
+  });
+
+export const nonEmptyStringMax100Trait =
+  NEStringAuFn.getTrait<NonEmptyStringMax100>({
+    max: 100,
+    message: 'should not be empty and large than 100',
+    exceptionCode: 'INVALID_NON_EMPTY_STRING_MAX_100',
+  });
