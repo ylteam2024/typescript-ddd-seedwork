0859c98f09a18d361afd886aa1b565eccfe4769b
p77u4n <tuancr.pt@gmail.com>
2024-02-26 22:53:35 +0700

    BaseTE + BaseEither + absortTE (for absording the result of TaskEither)

--- src/logic/fp.ts
+++ src/logic/fp.ts
@@ -6,6 +6,7 @@ import * as Apply from 'fp-ts/Apply';
 import * as O from 'fp-ts/Option';
 import * as Reader from 'fp-ts/Reader';
 import * as TaskEither from 'fp-ts/TaskEither';
+import * as Task from 'fp-ts/Task';
 import * as Either from 'fp-ts/Either';
 import * as IO from 'fp-ts/IO';
 import * as State from 'fp-ts/State';
@@ -16,7 +17,21 @@ import * as NUM from 'fp-ts/number';
 import * as RRecord from 'fp-ts/ReadonlyRecord';
 import * as io from 'io-ts';
 import * as IoTypes from 'io-ts-types';
+export * as rd from 'ramda';
 import { pipe, flow } from 'fp-ts/lib/function';
+import { BaseException } from './exception.base';
+import { ValidationErr } from '@model/invariant-validation';
+
+type SumException = BaseException | BaseException[] | ValidationErr;
+export type BaseTE<T> = TaskEither.TaskEither<SumException, T>;
+
+export type BaseEither<T> = Either.Either<SumException, T>;
+
+export const absordTE = <T extends TaskEither.TaskEither<any, any>>(te: T) =>
+  pipe(
+    te,
+    TaskEither.map(() => {}),
+  );
 
 export {
   Optics,
@@ -29,11 +44,12 @@ export {
   Reader,
   TaskEither as TE,
   IO,
+  IOEither,
   io,
+  Task,
   Arr,
   Either,
   NEA,
-  IOEither,
   State,
   RRecord,
   Record,
