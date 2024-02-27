d43947cb8c9b30a7e6a90f799328e7d39f63f06c
p77u4n <tuancr.pt@gmail.com>
2024-02-27 16:01:56 +0700

    Writable Type utility

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,8 @@
 export * from './Class';
 export * from './ExcludeFunctionProps';
 
+export type Writable<T> = {
+  -readonly [P in keyof T]: T[P];
+};
+
 export type Brand<K, T> = K & { __brand: T };
