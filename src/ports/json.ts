4250f6d86727ee14589f4fa2610f9458d53a6a7f
TuanCR <tuancr.pt@gmail.com>
2024-07-02 23:06:11 +0700

    baseexception for JSONUtil

--- src/ports/json.ts
+++ src/ports/json.ts
@@ -1,14 +1,16 @@
+import { BaseException, BaseExceptionBhv } from '@logic/exception.base';
 import { Either, tryCatch } from 'fp-ts/lib/Either';
 import { UnknownRecord } from 'type-fest';
 
 interface JsonUtil {
-  parse: <T = UnknownRecord>(s: string) => Either<Error, T>;
+  parse: <T = UnknownRecord>(s: string) => Either<BaseException, T>;
 }
 
 export const JsonUtil: JsonUtil = {
   parse: (s) =>
     tryCatch(
       () => JSON.parse(s),
-      (e) => e as Error,
+      (e) =>
+        BaseExceptionBhv.construct((e as Error).message, 'JSON_PARSE_FAILED'),
     ),
 };
