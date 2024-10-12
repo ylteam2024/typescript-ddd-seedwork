6caeca80af92d21c118b3ac3c39b843d34434ae8
TuanCR <tuancr.pt@gmail.com>
2024-10-12 23:45:35 +0700

    update NES code failure parsing

--- src/model/value-objects/NoneEmptyString.ts
+++ src/model/value-objects/NoneEmptyString.ts
@@ -3,7 +3,7 @@ import { Either, flow, pipe } from '@logic/fp';
 import { PrimitiveVOTrait } from '@model/value-object.base';
 import { Brand } from '@type_util/index';
 import { NonEmptyString as IONonEmptyString } from 'io-ts-types';
-import { ValidationErr, ValidationTrait } from '..';
+import { ValidationErr, ValidationErrTrait, ValidationTrait } from '..';
 
 export const isEmptyStringMaxNLength =
   <T>(maxLength: number) =>
@@ -46,7 +46,12 @@ export type NonEmptyStringMax100 = Brand<string, 'NonEmptyStringMax100'>;
 const parseNEString = (s: unknown) =>
   pipe(
     IONonEmptyString.decode(s),
-    ValidationTrait.fromIOValidation<NonEmptyString>,
+    Either.mapLeft(() =>
+      BaseExceptionTrait.construct(
+        'non empty string should have content',
+        'INVALID_NON_EMPTY_STRING',
+      ),
+    ),
   );
 
 export const NonEmptyStringTrait: PrimitiveVOTrait<
